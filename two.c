14c71a4c1fed926d08c81d0e327fb5d6e8d0af05
RAJESH T H <rajesh@pestrust.edu.in>
2024-09-11 16:25:58 +0530

    Your descriptive commit message

---
+++ two.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    char mainstr[50],pattern[50],replacement[50];
+	int pointm;
+    printf("enter the main string\n");
+	gets(mainstr);
+	printf("enter the pattern to be searched\n");  
+	gets(pattern);
+	printf("enter the replacement string\n");
+	gets(replacement); 
+    /* code */
+    return 0;
+}
